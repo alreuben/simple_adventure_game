d5a44d9e448f8269fe4cb41064089b9aace09441
alreuben <93282897+alreuben@users.noreply.github.com>
2024-09-19 18:09:17 +0100

    added text hekper for all questions and chnaged colours for questions

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ class Format:
     off = "\033[0m"
     red = "\033[91m"
     cyan = "\033[36m"
+    green = '\033[32m'
     bold = "\033[1m"
     italic = "\033[3m"
 
 
-answer = input("Would you like to play a game? (yes/no) ")
+answer = input(f"{Format.green}Would you like to play a game? {Format.italic}(yes / no){Format.off} ")
 
 if answer.lower().strip() == "yes":
     print("....") 
@@ -16,11 +17,14 @@ if answer.lower().strip() == "yes":
     print("Hello")
     sleep(2)
     print("How did you get here?")
-    sleep(4)
-    print(f"Welcome to my game of {Format.red}{Format.bold}choices{Format.off}")  
+    sleep(3)
+    print(f"Welcome to my game of {Format.red}{Format.bold}choices ( ͡° ͜ʖ ͡°){Format.off}")  
+    sleep(2)
     
-    answer = input(f"Look around you... You're in the woods. You see a sign. Left for the mountains, Right for the bridge. Where do you {Format.red}{Format.bold}chose?{Format.off} ")
+    #CPU Question -> Mountains (Left) or Bridge (Right)
+    answer = input(f"{Format.green}Look around you... You're in the woods. You see a sign. Left for the mountains, Right for the bridge. Where do you chose? {Format.italic}(Left / Right){Format.off}")
     
+    #OPTION -> LEFT -> MOUNTAINS
     if answer.lower().strip() == "left":
         print("You take a long walk up the mountains")
         sleep(3)
@@ -29,12 +33,29 @@ if answer.lower().strip() == "yes":
         print("... You try to listen out for more")
         sleep(1) 
         print("..") 
-        sleep(3)
+        sleep(2)
         print("....") 
         sleep(3)
         print("........") 
-        print("You hear nothing.")
-        sleep(1)
+        sleep(3)
+        print("You hear nothing but you remember the direction of the sound.")
+        sleep(3)
+        
+        #OPTION -> FOLLOW SOUND -> MEET LUMBERJACK
+        answer = input(f"{Format.green}Do you want to follow the sound or go in the opposite direction? {Format.italic}(Follow / Opposite){Format.off}") #add something about exploring
+        if "follow" in answer.lower().strip() or "sound" in answer.lower().strip():
+            print("You decide to follow the sound you heard earlier")   
+            print("Standing behind you is a tall bearded man holding an axe...")
+            sleep(3)
+            print("Covered in blood")
+
+        if "opposite" in answer.lower().strip() or "explore" in answer.lower().strip():
+            print(f"{Format.italic}{Format.cyan}I better keep my distance{Format.off} You mumble to yourself") 
+            sleep(3)
+            print("You try to make your way through the mountains")   
+          
+    
+    #OPTION -> RIGHT -> BRIDGE
     elif answer.lower().strip() == 'right':
         print("You turn right and look out for a bridge")
         sleep(3)
@@ -43,53 +64,59 @@ if answer.lower().strip() == "yes":
         print(f"{Format.italic}{Format.cyan}Where is this bridge?{Format.off} You think to yourself")
         sleep(3) 
         print("You keep walking and feel something hard beneath your feet") 
-       
-        answer = input("Do yo want to keep searching for the bridge? Or do you want to investigate the floor? (search/investigate)")
+
+        #CPU Question -> Keep going on bridge (Bridge) or Look at the floor (Investigate)
+        answer = input(f"{Format.green}Do yo want to keep searching for the bridge? Or do you want to investigate the floor? {Format.italic}(Search / Investigate){Format.off}")
         if "bridge" in answer.lower().strip() or "search" in answer.lower().strip():
-            print("You ignore the feeling under your feet and keep walking")
+            print("You ignore the feeling under your feet and keep walking.")
             sleep(3)
-            print("You see a bright light in the far distance... The bridge is right in front of you")
+            print("You see a bright light in the far distance... The bridge is right in front of you.")
             sleep(3)
-            print("As you run towards the light your body starts to lean forward") 
+            print("As you run towards the light your body starts to lean forward.") 
             sleep(3)
             print(f"{Format.italic}{Format.cyan}Huh{Format.off}") 
             sleep(3)
-            print("you look around.. the bridge is above you")  
+            print("you look around.. the bridge is above you.")  
             sleep(3)
             print(f"{Format.italic}{Format.cyan}THE BRIDGE WAS BROKEN?!{Format.off}") 
             sleep(3)
             print(" you fall..")
+            sleep(5)
+            print("you die.")
             sleep(3)
-            print("you die")
-            sleep(3)
-            print(f"{Format.red}{Format.bold}𓁹‿𓁹 GAME OVER 𓁹‿𓁹{Format.off}")
+            print(f"{Format.red}{Format.bold}◉‿◉ GAME OVER ◉‿◉{Format.off}")
             
         if "floor" in answer.lower().strip() or "investigate" in answer.lower().strip():
-            print("You search the floor in a panic and pick up the hard piece of metal")
+            print("You search the floor in a panic and pick up the hard piece of metal that was under your feet.")
             sleep(3)
-            print("You wipe it clean as you bring it closer to your face")
+            print("You wipe away the dirt and bring it closer to your face.")
             sleep(3)
             print(f"{Format.italic}{Format.red}'--R-I-G - B--DG- ----EN UP A---D'{Format.off}")
             sleep(3)
             print(f"{Format.italic}{Format.cyan}Huh? What does that say?{Format.off}")
             sleep(3)
-            print("You rub your eyes and try to focus on the writing")
+            print("You rub your eyes and try to focus on the writing.")
             sleep(3)
             print(f"{Format.italic}{Format.red}'WARNING - BRIDGE BROKEN UP AHEAD'{Format.off}")
             sleep(3)
             print("You gasp and take a step back")
             sleep(3)
-            print(f"{Format.italic}{Format.cyan}Thank goodness I didn't cross that bridge...{Format.off} You think to yourself")
+            print(f"{Format.italic}{Format.cyan}Thank goodness I didn't cross that bridge...{Format.off} You think to yourself.")
         
     else:
         print("...") 
         sleep(1)
-        print(f"That wasn't an option.{Format.red}{Format.bold}YOU'VE LOST{Format.off}") 
+        print(f"That wasn't an option.") 
         sleep(2)
+        print(f"Maybe next time you'll learn to follow instructions!") 
+        sleep(2)
+        print(f"{Format.red}{Format.bold}YOU'VE LOST{Format.off}") 
+        sleep(2)
+        print(f"{Format.red}{Format.bold}◉‿◉ GAME OVER ◉‿◉{Format.off}")
 
 else:
     print("..") 
     sleep(1)
     print("...") 
     sleep(2)
-    print(f"{Format.red}{Format.bold}𓁹‿𓁹I guess you'll die here then 𓁹‿𓁹{Format.off}") 
+    print(f"{Format.red}{Format.bold}( ͡° ͜ʖ ͡°) I guess you'll die here then ( ͡° ͜ʖ ͡°){Format.off}") 
